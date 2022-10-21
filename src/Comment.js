fde6df2f7ed036b6d21936349bd5a44a76c963fb
Panindra Chekoori <pxchek.developer@gmail.com>
2022-10-21 13:13:17 -0500

    Add Form component

--- src/Comment.js
+++ src/Comment.js
@@ -1,16 +1,21 @@
 import React, {Component} from "react";
 import UserInfo from "./UserInfo";
 import Tick from "./Tick";
+import NameForm from "./Nameform";
 
 class Comment extends Component {
     render() {
         return (
+            <div>
+                <p>Submit Form</p>
+                <NameForm/>
             <div className="Comment">
                 <UserInfo user={this.props.author} />
                 <div className="Comment-text">
                     {this.props.text}
                 </div>
                 <Tick/>
+            </div>
             </div>
         );
     }
