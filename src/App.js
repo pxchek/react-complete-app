2cd3500ff8d239b10e70af1dd60e6b11c3cf5e51
Panindra Chekoori <pxchek.developer@gmail.com>
2022-11-10 21:14:16 -0600

    Add support components

--- src/App.js
+++ src/App.js
@@ -2,53 +2,26 @@ import React from "react";
 import {SignUp} from "./login/SignUp";
 import {Header} from "./header/Header";
 import {Footer} from "./footer/Footer";
-import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Cart} from "./Cart";
 import {Checkout} from "./Checkout";
 import {Product} from "./Product";
 import {Confirmation} from "./Confirmation";
+import {Home} from "./Home";
 
 export const App = () => {
     return (
-        <div align="center">
-            <BrowserRouter>
-                <Header/>
-                <ul>
-                    <li>
-                        <Link to="/checkLogin">CheckLogin</Link>
-                    </li>
-                    <li>
-                        <Link to="/product">Product</Link>
-                    </li>
-                    <li>
-                        <Link to="/cart">Cart</Link>
-                    </li>
-                    <li>
-                        <Link to="/checkout">Checkout</Link>
-                    </li>
-                    <li>
-                        <Link to="/confirmation">Confirmation</Link>
-                    </li>
-                </ul>
-                <Switch>
-                    <Route path="/cart">
-                        <Cart/>
-                    </Route>
-                    <Route path="/checkLogin">
-                        <SignUp/>
-                    </Route>
-                    <Route path="/checkout">
-                        <Checkout/>
-                    </Route>
-                    <Route path="/product">
-                        <Product/>
-                    </Route>
-                    <Route path="/confirmation">
-                        <Confirmation/>
-                    </Route>
-                </Switch>
-                <Footer/>
-            </BrowserRouter>
-        </div>
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/cart" element={<Cart/>}/>
+                <Route path="/checkLogin" element={<SignUp/>}/>
+                <Route path="/checkout" element={<Checkout/>}/>
+                <Route path="/product" element={<Product/>}/>
+                <Route path="/confirmation" element={<Confirmation/>}/>
+            </Routes>
+            <Footer/>
+        </BrowserRouter>
     );
 };
