8e43657f6cc5b3717d631563a5e80c32706dfa78
Panindra Chekoori <pxchek.developer@gmail.com>
2022-11-10 21:46:00 -0600

    Align the components to center

--- src/App.js
+++ src/App.js
@@ -11,17 +11,19 @@ import {Home} from "./components/homepage/Home";
 
 export const App = () => {
     return (
-        <BrowserRouter>
-            <Header/>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/cart" element={<Cart/>}/>
-                <Route path="/checkLogin" element={<SignUp/>}/>
-                <Route path="/checkout" element={<Checkout/>}/>
-                <Route path="/product" element={<Product/>}/>
-                <Route path="/confirmation" element={<Confirmation/>}/>
-            </Routes>
-            <Footer/>
-        </BrowserRouter>
+        <div align="center">
+            <BrowserRouter>
+                <Header/>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/cart" element={<Cart/>}/>
+                    <Route path="/checkLogin" element={<SignUp/>}/>
+                    <Route path="/checkout" element={<Checkout/>}/>
+                    <Route path="/product" element={<Product/>}/>
+                    <Route path="/confirmation" element={<Confirmation/>}/>
+                </Routes>
+                <Footer/>
+            </BrowserRouter>
+        </div>
     );
 };
